b6e8241a706869c5970c0bf3cb34eecc55feaeb4
The Kneb <sleepwell95@gmail.com>
2019-10-24 21:12:47 +0300

    Testing parralax with JS. Added some placeholhers.

---
+++ Source Code/javaConfig.js
@@ -0,0 +1,6 @@
+const parallax1 = document.getElementById("section1");
+
+window.addEventListener("scroll", function() {
+  let offset = window.pageYOffset;
+  parallax1.style.backgroundPositionY = offset * 0.6 + "px";
+});
