0bacca9742aab525960d6100f3a301746f323cb2
The Kneb <sleepwell95@gmail.com>
2019-10-24 21:14:22 +0300

    Some Parallax changes.

--- Source Code/javaConfig.js
+++ Source Code/javaConfig.js
@@ -2,5 +2,5 @@ const parallax1 = document.getElementById("section1");
 
 window.addEventListener("scroll", function() {
   let offset = window.pageYOffset;
-  parallax1.style.backgroundPositionY = offset * 0.6 + "px";
+  parallax1.style.backgroundPositionY = offset * 0.5 + "px";
 });
