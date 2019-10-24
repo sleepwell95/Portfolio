8d254b9b60bf7f06930c1512dad5cd73af2ba42c
The Kneb <sleepwell95@gmail.com>
2019-10-24 22:06:17 +0300

    Failų konfigūracija ir formatavimas. Reikės perdaryti parallax scriptą.

--- Source Code/javaConfig.js
+++ Source Code/javaConfig.js
@@ -1,6 +1,7 @@
-const parallax1 = document.getElementById("section1");
+var parallax1 = document.getElementById("section1");
 
 window.addEventListener("scroll", function() {
-  let offset = window.pageYOffset;
-  parallax1.style.backgroundPositionY = offset * 0.5 + "px";
+  var offset = window.pageYOffset;
+
+  parallax1.style.backgroundPositionY = offset * 0.7 + "px";
 });
